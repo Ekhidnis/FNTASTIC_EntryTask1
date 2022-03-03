0a62520c8144875279b763aa9298b6549983c587
Ekhidnis <ekhidnis@gmail.com>
2022-03-03 21:19:21 +0700

    Initial

---
+++ InputConverter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string str = "";
+
+	std::cout << "Please, enter the string you want to convert: " << std::endl;
+	std::cin >> str;
+	str::cout << "Converted string: " << str << std::endl;
+
+	return 0;
+}
